308cc6180dcbe735c003370f7ad8771140d0d8bb
Nick Phelps <nickphelps34@gmail.com>
2022-10-14 10:41:34 -0400

    small console.log changes

--- js/challenge.js
+++ js/challenge.js
@@ -2,5 +2,6 @@ const minusButton = document.getElementById("minus");
 const counter = document.getElementById("counter");
 
 minusButton.addEventListener("click", function() {
-    const counterValue = Math(counter.textContent) ++;
+    const counterValue = Number(counter.textContent()) ++;
+    console.log(counterValue)
 })
