d0175548278b278699ffb32715b3e469a261b169
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-10-14 10:22:44 -0500

    add plus button and interval

--- js/challenge.js
+++ js/challenge.js
@@ -1,7 +1,29 @@
+const addButton = document.getElementById("plus");
 const minusButton = document.getElementById("minus");
 const counter = document.getElementById("counter");
+let counterValue = 0/*Number(counter.textContent()) ++;*/
+function increaseCounter() {counterValue ++;
+counter.textContent = counterValue;}
 
-minusButton.addEventListener("click", function() {
-    const counterValue = Number(counter.textContent()) ++;
-    console.log(counterValue)
-})
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    counter.innerHTML = counterValue;
+    
+    
+    setInterval(increaseCounter, 1000)
+    
+    minusButton.addEventListener("click", function() {
+        // console.log(counterValue)
+        
+        counter.innerHTML = counterValue --;
+
+});
+
+addButton.addEventListener("click", function() {
+    // console.log(counterValue)
+    
+    counter.innerHTML = counterValue ++;
+ 
+ });})
