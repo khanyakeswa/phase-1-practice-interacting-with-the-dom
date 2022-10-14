906b3afdb213b11dc87c9b2d8b8c1c066c778a0d
Khanya <khanya.keswa@gmail.com>
2022-10-14 10:29:43 -0500

    challenge.js reformatted

--- js/challenge.js
+++ js/challenge.js
@@ -1,31 +1,25 @@
-const addButton = document.getElementById("plus");
-const minusButton = document.getElementById("minus");
-const counter = document.getElementById("counter");
-let counterValue = 0/*Number(counter.textContent()) ++;*/
+const addButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const counter = document.getElementById('counter')
+let counterValue = 0 /*Number(counter.textContent()) ++;*/
 function increaseCounter() {
-    counterValue ++;
-    counter.textContent = counterValue;
+  counterValue++
+  counter.textContent = counterValue
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  counter.innerHTML = counterValue
 
+  setInterval(increaseCounter, 1000)
 
-document.addEventListener('DOMContentLoaded', function() {
-    
-    counter.innerHTML = counterValue;
-    
-    
-    setInterval(increaseCounter, 1000)
-    
-    minusButton.addEventListener("click", function() {
-        // console.log(counterValue)
-        
-        counter.innerHTML = -- counterValue;
+  minusButton.addEventListener('click', function () {
+    // const counterValue = Number(counter.textContent()) ++;
+    console.log(counterValue)
+  })
+})
 
-});
+addButton.addEventListener('click', function () {
+  // console.log(counterValue)
 
-addButton.addEventListener("click", function() {
-    // console.log(counterValue)
-    
-    counter.innerHTML = ++ counterValue;
- 
- });})
+  counter.innerHTML = counterValue++
+})
